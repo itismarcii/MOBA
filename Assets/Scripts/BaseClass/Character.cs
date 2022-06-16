ef749c6c31055a18feac15b5c57ca2c2c5cfadb0
itismarcii <58007324+itismarcii@users.noreply.github.com>
2022-06-16 16:33:26 +0200

    Struct for Character information

--- Assets/Scripts/BaseClass/Character.cs
+++ Assets/Scripts/BaseClass/Character.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+///using System.SkillMachine;
 using System.StateMachine;
 using UnityEngine;
 using UnityEngine.AI;
@@ -16,38 +17,46 @@ namespace BaseClass
             _Team1_,
             _Team2_
         }
-
-        private StateMachine StateMachine;
         
-        private string CharacterName = "";
-        [SerializeField, Space(10)] private Team TeamEnum;
-
-        private bool IsAlive = true;
-
-        private int MaxHealth;
-        [SerializeField] private int Health;
-        private int Armor;
-        private int MagicResist;
-
-        private int AttackPower;
-        private int MagicPower;
+        [Serializable]
+        internal struct Info
+        {
+            internal StateMachine StateMachine;
+            //[SerializeField, Space(10)] internal SkillTree SkillTree;
         
-        private int CoolDown;
-        private int AttackSpeed;
-        private int CritChance;
-        private float MovementSpeed;
-        private int LifeSteal;
-        private Element[] Elements;
+            internal string CharacterName;
+            [SerializeField] internal Team TeamEnum;
 
-        private int Level;
+            internal bool IsAlive;
 
-        private Effect Passive;
-        private Effect[] Abilities;
-        private Spell[] Skills;
+            [SerializeField] internal int MaxHealth;
+            [SerializeField] internal int Health;
+            internal int Armor;
+            internal int MagicResist;
 
-        private NavMeshAgent Agent;
-        private Rigidbody Rigidbody;
-        private Collider Collider;
+            internal int AttackPower;
+            internal int MagicPower;
+        
+            internal int CoolDown;
+            internal int AttackSpeed;
+            internal int CriticalChance;
+            internal float MovementSpeed;
+            internal int LifeSteal;
+            internal Element[] Elements;
+
+            internal int Level;
+
+            internal Effect Passive;
+            internal Effect[] Abilities;
+            internal Spell[] Skills;
+
+            internal NavMeshAgent Agent;
+            internal Rigidbody Rigidbody;
+            internal Collider Collider;
+        }
+
+        [SerializeField] private Info Information;
+        
 
         void Awake()
         {
@@ -56,102 +65,109 @@ namespace BaseClass
         
         public override void _Awake_()
         {
-            StateMachine = GetComponent<StateMachine>();
-            Rigidbody = GetComponent<Rigidbody>();
-            Collider = GetComponent<Collider>();
-            Rigidbody.isKinematic = true;
-            Agent = GetComponent<NavMeshAgent>();
-            Agent.angularSpeed = 1000;
-            Agent.acceleration = 200;
+            Information.StateMachine = GetComponent<StateMachine>();
+            Information.Rigidbody = GetComponent<Rigidbody>();
+            Information.Collider = GetComponent<Collider>();
+            Information.Rigidbody.isKinematic = true;
+            Information.Agent = GetComponent<NavMeshAgent>();
+            Information.Agent.angularSpeed = 1000;
+            Information.Agent.acceleration = 200;
 
             transform.tag = "Character";
+            Information.SkillTree.ConfigSkills();
         }
 
         public override void _Update_()
         {
-            base._Update_();
+            Information.SkillTree.RunPassiveSkills();
+            CheckDeath();
+        }
+
+        private void CheckDeath()
+        {
             if (!CanTransitionTo(CharacterState._Death_)) return;
             ChangeCurrentState(CharacterState._Death_);
-            Rigidbody.Sleep();
-            Agent.enabled = false;
-            Collider.enabled = false;
+            Information.Rigidbody.Sleep();
+            Information.Agent.enabled = false;
+            Information.Collider.enabled = false;
         }
 
 
         #region Getter & Setter
 
         // GETTER
-        internal CharacterState GetCurrentState() => StateMachine.GetCurrentState();
-        internal string GetCharacterName() => CharacterName;
-        internal Team GetTeam() => TeamEnum;
-        internal bool GetIsAlive() => IsAlive;
-        internal int GetMaxHealth() => MaxHealth;
-        internal int GetHealth() => Health;
-        internal int GetArmor() => Armor;
-        internal int GetMagicResist() => MagicResist;
-        internal int GetAttackPower() => AttackPower;
-        internal int GetMagicPower() => MagicPower;
-        internal int GetCoolDown() => CoolDown;
-        internal int GetAttackSpeed() => AttackSpeed;
-        internal int GetCritChange() => CritChance;
-        internal float GetMovementSpeed() => MovementSpeed;
-        internal int GetLifeSteal() => LifeSteal;
-        internal Element[] GetElements() => Elements;
-        internal int GetLevel() => Level;
-        internal Effect GetPassive() => Passive;
-        internal Effect[] GetAbilities() => Abilities;
-        internal Spell[] GetSkills() => Skills;
-        internal NavMeshAgent GetAgent() => Agent;
+        internal CharacterState GetCurrentState() => Information.StateMachine.GetCurrentState();
+        //internal SkillTree GetSkillTree() => Information.SkillTree;
+        internal string GetCharacterName() => Information.CharacterName;
+        internal Team GetTeam() => Information.TeamEnum;
+        internal bool GetIsAlive() => Information.IsAlive;
+        internal int GetMaxHealth() => Information.MaxHealth;
+        internal int GetHealth() => Information.Health;
+        internal int GetArmor() => Information.Armor;
+        internal int GetMagicResist() => Information.MagicResist;
+        internal int GetAttackPower() => Information.AttackPower;
+        internal int GetMagicPower() => Information.MagicPower;
+        internal int GetCoolDown() => Information.CoolDown;
+        internal int GetAttackSpeed() => Information.AttackSpeed;
+        internal int GetCritChange() => Information.CriticalChance;
+        internal float GetMovementSpeed() => Information.MovementSpeed;
+        internal int GetLifeSteal() => Information.LifeSteal;
+        internal Element[] GetElements() => Information.Elements;
+        internal int GetLevel() => Information.Level;
+        internal Effect GetPassive() => Information.Passive;
+        internal Effect[] GetAbilities() => Information.Abilities;
+        internal Spell[] GetSkills() => Information.Skills;
+        internal NavMeshAgent GetAgent() => Information.Agent;
 
         // SETTER
-        internal bool ChangeCurrentState(CharacterState state) => StateMachine.ChangeState(state);
-        internal bool CanTransitionTo(CharacterState state) => StateMachine.TransitionAllowance(state);
-        internal bool ChangeCurrentState(CharacterState state, float duration) => StateMachine.ChangeState(state, duration);
-        internal void SetCharacterName(string characterName) => CharacterName = characterName;
-        internal void SetTeam(Team team) => TeamEnum = team;
-        internal void SetIsAlive(bool alive) => IsAlive = alive;
-        internal void SetMaxHealth(int health) => MaxHealth = health;
-        internal void SetHealth(int health) => Health = health;
-        internal void SetArmor(int armor) => Armor = armor;
-        internal void SetMagicResist(int magicResist) => MagicResist = magicResist;
-        internal void SetAttackPower(int attackPower) => AttackPower = attackPower;
-        internal void SetMagicPower(int magicPower) => MagicPower = magicPower;
-        internal void SetCoolDown(int coolDown) => CoolDown = coolDown;
-        internal void SetAttackSpeed(int attackSpeed) => AttackSpeed = attackSpeed;
-        internal void SetCritChance(int critChance) => CritChance = critChance;
-        internal void SetMovementSpeed(float movementSpeed) => MovementSpeed = movementSpeed;
-        internal void SetLifeSteal(int lifeSteal) => LifeSteal = lifeSteal;
-        internal void SetElements(Element[] elements) => Elements = elements;
+        internal bool ChangeCurrentState(CharacterState state) => Information.StateMachine.ChangeState(state);
+        internal bool CanTransitionTo(CharacterState state) => Information.StateMachine.TransitionAllowance(state);
+        internal bool ChangeCurrentState(CharacterState state, float duration) => Information.StateMachine.ChangeState(state, duration);
+        internal void SetCharacterName(string characterName) => Information.CharacterName = characterName;
+        internal void SetTeam(Team team) => Information.TeamEnum = team;
+        internal void SetIsAlive(bool alive) => Information.IsAlive = alive;
+        internal void SetMaxHealth(int health) => Information.MaxHealth = health;
+        internal void SetHealth(int health) => Information.Health = health;
+        internal void SetArmor(int armor) => Information.Armor = armor;
+        internal void SetMagicResist(int magicResist) => Information.MagicResist = magicResist;
+        internal void SetAttackPower(int attackPower) => Information.AttackPower = attackPower;
+        internal void SetMagicPower(int magicPower) => Information.MagicPower = magicPower;
+        internal void SetCoolDown(int coolDown) => Information.CoolDown = coolDown;
+        internal void SetAttackSpeed(int attackSpeed) => Information.AttackSpeed = attackSpeed;
+        internal void SetCriticalChance(int criticalChance) => Information.CriticalChance = criticalChance;
+        internal void SetMovementSpeed(float movementSpeed) => Information.MovementSpeed = movementSpeed;
+        internal void SetLifeSteal(int lifeSteal) => Information.LifeSteal = lifeSteal;
+        internal void SetElements(Element[] elements) => Information.Elements = elements;
         internal void SetElement(Element element)
         {
-            var elements = Elements.ToList();
+            var elements = Information.Elements.ToList();
             elements.Add(element);
-            Elements = elements.ToArray();
+            Information.Elements = elements.ToArray();
         } 
         internal void RemoveElement(Element element)
         {
-            var elements = Elements.ToList();
+            var elements = Information.Elements.ToList();
             elements.Remove(element);
-            Elements = elements.ToArray();
+            Information.Elements = elements.ToArray();
         }
-        internal void SetLevel(int level) => Level = level;
-        internal void SetPassive(Effect passive) => Passive = passive;
-        internal void SetAbilities(Effect[] abilities) => Abilities = abilities;
-        internal void SetSkills(Spell[] skills) => Skills = skills;
+        internal void SetLevel(int level) => Information.Level = level;
+        internal void SetPassive(Effect passive) => Information.Passive = passive;
+        internal void SetAbilities(Effect[] abilities) => Information.Abilities = abilities;
+        internal void SetSkills(Spell[] skills) => Information.Skills = skills;
         internal void SetSkill(Spell effect)
         {
-            var skills = Skills.ToList();
+            var skills = Information.Skills.ToList();
             skills.Add(effect);
-            Skills = skills.ToArray();
+            Information.Skills = skills.ToArray();
         }
         internal void RemoveSkill(Spell effect)
         {
-            var skills = Skills.ToList();
+            var skills = Information.Skills.ToList();
             skills.Remove(effect);
-            Skills = skills.ToArray();
+            Information.Skills = skills.ToArray();
         }
 
-        internal void SetWalkAndCast(bool state) => StateMachine.SetCastAndWalk(state);
+        internal void SetWalkAndCast(bool state) => Information.StateMachine.SetCastAndWalk(state);
 
         #endregion
 
@@ -228,41 +244,42 @@ namespace BaseClass
 
         internal void Revive(Vector3 position)
         {
-            ChangeCurrentState(CharacterState._Idle_);
+            //ChangeCurrentState(CharacterState._Revive_);
             SetHealth(GetMaxHealth());
             transform.position = position;
-            Agent.enabled = true;
-            Collider.enabled = true;
-            Agent.SetDestination(position);
+            Information.Agent.enabled = true;
+            Information.Collider.enabled = true;
+            Information.Agent.SetDestination(position);
+            ChangeCurrentState(CharacterState._Idle_);
         }
 
         private void ReadInformation(TextAsset characterJson)
         {
             var character = JsonUtility.FromJson<Character>(characterJson.text);
 
-            CharacterName = character.CharacterName;
+            Information.CharacterName = character.Information.CharacterName;
 
-            IsAlive = character.IsAlive;
+            Information.IsAlive = character.Information.IsAlive;
 
-            Health = character.Health;
-            Armor = character.Armor;
-            MagicResist = character.MagicResist;
+            Information.Health = character.Information.Health;
+            Information.Armor = character.Information.Armor;
+            Information.MagicResist = character.Information.MagicResist;
 
-            AttackPower = character.AttackPower;
-            MagicPower = character.MagicPower;
+            Information.AttackPower = character.Information.AttackPower;
+            Information.MagicPower = character.Information.MagicPower;
 
-            CoolDown = character.CoolDown;
-            AttackSpeed = character.AttackSpeed;
-            CritChance = character.CritChance;
-            MovementSpeed = character.MovementSpeed;
-            LifeSteal = character.LifeSteal;
-            Elements = character.Elements;
+            Information.CoolDown = character.Information.CoolDown;
+            Information.AttackSpeed = character.Information.AttackSpeed;
+            Information.CriticalChance = character.Information.CriticalChance;
+            Information.MovementSpeed = character.Information.MovementSpeed;
+            Information.LifeSteal = character.Information.LifeSteal;
+            Information.Elements = character.Information.Elements;
 
-            Level = character.Level;
+            Information.Level = character.Information.Level;
 
-            Passive = character.Passive;
-            Abilities = character.Abilities;
-            Skills = character.Skills;
+            Information.Passive = character.Information.Passive;
+            Information.Abilities = character.Information.Abilities;
+            Information.Skills = character.Information.Skills;
         }
 
         #endregion
